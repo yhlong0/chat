46adc19e90a5cd2a92882241c79e4c7770b75b4f
Hailong Yang <hyang@ESHIPGLOBAL.LOCAL>
2018-04-16 14:07:24 -0500

    add js for ui

---
+++ public/javascripts/chat.js
@@ -0,0 +1,17 @@
+var Chat = function(socket) {
+    this.socket = socket;
+};
+
+Chat.prototype.sendMessage = function(room, text) {
+    var message = {
+        room: room,
+        text: text
+    };
+    this.socket.emit('message', message);
+};
+
+Chat.prototype.changeRoom = function(room) {
+    tihs.socket.emit('join', {
+        newRoom: room
+    });
+};
